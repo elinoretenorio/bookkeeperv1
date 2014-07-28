2fac69d80a7dad1fe8687569a41202c052870c05
Elinore Tenorio <elinore.tenorio@gmail.com>
2014-07-28 12:08:37 +0800

    Added a default page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+
+<div class="row">
+  <div class="col-md-6 col-md-offset-4">
+    <form class="form-inline" role="form" method="get" action="home.php">
+    <div class="form-group">
+      <label>Enter ISBN</label>
+      <input type="text" class="form-control" name="isbn" id="isbn">
+      <input type="submit" class="btn btn-primary" value="Go" />
+    </div>
+    </form>
+  </div>
+</div>
+
+<p>&nbsp;</p>
+<?php include 'footer.php'; ?>
